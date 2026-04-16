d4923714c58fbe4c4605431111d9744bff3d460e
FatboyFrostiie <fatboysapple98@gmail.com>
2026-04-16 13:54:49 -0500

    Create ShopService.lua in src/ReplicatedStorage/Modules

---
+++ src/ReplicatedStorage/Modules/ShopService.lua
@@ -0,0 +1,9 @@
+local ShopService = {}
+
+ShopService.Items = {
+    NeonTrail = { Id = "NeonTrail", Name = "Neon Dash Trail", Type = "AbilitySkin", AbilityId = "Dash", Price = 1000, Premium = false },
+    GlitchAura = { Id = "GlitchAura", Name = "Glitch Aura", Type = "Aura", Price = 200, Premium = true },
+    FragmentBooster = { Id = "FragmentBooster", Name = "Fragment Booster x2 (15 min)", Type = "Booster", Price = 150, Premium = true },
+}
+
+return ShopService
